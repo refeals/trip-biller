fb0e96e38276f451323b6de0279955bb0224c58d
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2025-05-13 17:19:57 -0300

    feat: add dark mode and main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
+import { ThemeProvider } from "@/components/theme-provider"
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   title: "Trip Biller",
@@ -19,16 +9,23 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="pt-BR">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <>
+      <html lang="pt-BR" suppressHydrationWarning>
+        <body className="min-h-[100svh] grid">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   )
 }
