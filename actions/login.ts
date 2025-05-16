e0e7460167c6a1800f34a6062e41d34fca22da31
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2025-05-16 16:06:00 -0300

    hotfix

--- actions/login.ts
+++ actions/login.ts
@@ -1,40 +1,40 @@
-"use server"
+// "use server"
 
-import { PrismaClient } from "@/generated/prisma"
-import { cookies } from "next/headers"
+// import { PrismaClient } from "@/generated/prisma"
+// import { cookies } from "next/headers"
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
-export const login = async (formData: FormData) => {
-  const c = await cookies()
-  const email = formData.get("email") as string
+// export const login = async (formData: FormData) => {
+//   const c = await cookies()
+//   const email = formData.get("email") as string
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
+//   const user = await prisma.user.findUnique({
+//     where: {
+//       email,
+//     },
+//   })
 
-  if (user) {
-    c.set({
-      name: "userId",
-      value: user.id,
-    })
+//   if (user) {
+//     c.set({
+//       name: "userId",
+//       value: user.id,
+//     })
 
-    return { success: true, user }
-  }
+//     return { success: true, user }
+//   }
 
-  const newUser = await prisma.user.create({
-    data: {
-      email,
-      name: email.split("@")[0],
-    },
-  })
+//   const newUser = await prisma.user.create({
+//     data: {
+//       email,
+//       name: email.split("@")[0],
+//     },
+//   })
 
-  c.set({
-    name: "userId",
-    value: newUser.id,
-  })
+//   c.set({
+//     name: "userId",
+//     value: newUser.id,
+//   })
 
-  return { success: true, user: newUser }
-}
+//   return { success: true, user: newUser }
+// }
